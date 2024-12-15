0e72ac66cfa2abd802754f6d16efcdeff6b27b6e
dSamp41 <d.sampietro@studenti.unipi.it>
2024-12-15 16:02:15 +0100

    Implement register, label, register_table

---
+++ lib/register.ml
@@ -0,0 +1,40 @@
+type register = 
+  | Register of int
+
+let string_of_register (r: register) : string =
+  match r with 
+  | Register(n) -> "r" ^ (string_of_int n)
+
+
+let register_counter = ref (-1) (* first register is r0*)
+let get_new_register () : register = 
+  incr register_counter;
+  Register(!register_counter)
+
+
+type label = string
+let label_counter = ref 0
+let get_new_label () : label =
+  incr label_counter;
+  "L" ^ (string_of_int !label_counter)
+
+(* Map(Var -> Register) *)
+(* reg_table is an Hashtbl because we need in-place mutability *)
+type register_table = (string, register) Hashtbl.t
+
+let new_table (n: int) : register_table = Hashtbl.create n
+
+(* Update the value of a variable in memory *)
+let lookup (rt: register_table) (id: string): register option =
+  Hashtbl.find_opt rt id 
+
+let lookup_or_add (rt: register_table) (id: string): register =
+  match Hashtbl.find_opt rt id with
+  | Some(reg) -> reg
+  | None -> 
+    let reg = get_new_register () in
+    Hashtbl.add rt id reg;
+    reg
+
+let add (id: string) (reg: register) (rt: register_table) = 
+  Hashtbl.add rt id reg 
---
+++ lib/register.mli
@@ -0,0 +1,14 @@
+type register = 
+  | Register of int
+
+val string_of_register: register -> string
+val get_new_register: unit -> register
+
+type label = string
+val get_new_label: unit -> label
+
+type register_table
+val new_table: int -> register_table
+val lookup: register_table -> string -> register option
+val lookup_or_add: register_table -> string -> register
+val add: string -> register -> register_table -> unit
