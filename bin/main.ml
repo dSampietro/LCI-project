8e7db54bf2149256152efee7e1e4656635dcb02a
dSamp41 <d.sampietro@studenti.unipi.it>
2024-12-16 16:03:57 +0100

    Update main.ml

--- bin/main.ml
+++ bin/main.ml
@@ -1,24 +1,40 @@
 
-
 let () = 
   let p = 
     Lib.MiniImp.Seq(
-      Lib.MiniImp.Assign("x", Lib.MiniImp.Aval(2)),
-      Lib.MiniImp.If(Lib.MiniImp.Minor(Lib.MiniImp.Var("y"), Lib.MiniImp.Aval(0)),
       Lib.MiniImp.Seq(
-          Lib.MiniImp.Assign(
-            "y", Lib.MiniImp.Plus(Lib.MiniImp.Var("x"), Lib.MiniImp.Aval(3))),
+        Lib.MiniImp.Assign("x", Lib.MiniImp.Aval(2)),
+        Lib.MiniImp.Assign("y", Lib.MiniImp.Aval(1))
+      ),
+      Lib.MiniImp.If(
+        Lib.MiniImp.Minor(Lib.MiniImp.Var("y"), Lib.MiniImp.Aval(0)),
+        Lib.MiniImp.Seq(
+          Lib.MiniImp.Assign( "y", Lib.MiniImp.Plus(Lib.MiniImp.Var("x"), Lib.MiniImp.Aval(3))),
           Lib.MiniImp.Assign("x", Lib.MiniImp.Var("y"))
         ),
-        Lib.MiniImp.Assign(
-          "x", Lib.MiniImp.Minus(Lib.MiniImp.Aval(1), Lib.MiniImp.Var("y"))
+        Lib.MiniImp.Seq(
+          Lib.MiniImp.Assign("y", Lib.MiniImp.Aval(10000)),
+          Lib.MiniImp.Assign( "x", Lib.MiniImp.Minus(Lib.MiniImp.Aval(1), Lib.MiniImp.Var("y")))
         )
       )
     )
+  in
+  let g = Lib.Generate_v2.program_to_cfg p in
+  let g = Lib.Translate_cfg.translate_cfg g in
+  Lib.MiniRisc_cfg.pp_cfg g
+  
+  
 
-  in 
-  let (g, _) = Lib.Generate.generate_cfg (Lib.CFG.empty()) p 0 
-  in Lib.CFG_pp.pp_cfg g
-(*
-in Printf.printf "#nodes: %d\n" (List.length (Lib.CFG.get_nodes g))
-*)
+  (*
+  let p =
+    Lib.MiniImp.Seq(
+      Lib.MiniImp.Assign("x", Lib.MiniImp.Aval(0)),
+      Lib.MiniImp.While(
+        Lib.MiniImp.Minor(Var("x"), Aval(10)),
+        Lib.MiniImp.Assign("x", Lib.MiniImp.Plus(Lib.MiniImp.Var("x"), Lib.MiniImp.Aval(1)))
+      )
+    )
+  in
+  let code = Lib.Translate.stmt_translate (Lib.Register.new_table 10) p
+  in print_string (Lib.Translate.get_code_string code)
+  *)
