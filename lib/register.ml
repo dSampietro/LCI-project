4c63066405d2f1d9d52554d30445e06959dcbdfd
dSamp41 <d.sampietro@studenti.unipi.it>
2024-12-31 17:07:05 +0100

    Update register.ml

--- lib/register.ml
+++ lib/register.ml
@@ -3,7 +3,11 @@ type register = int
   | Register of int
   *)
 
-let string_of_register (r: register) : string =  "r" ^ (string_of_int r)
+let string_of_register (r: register) : string =  
+  match r with
+  | -1 -> "rA"
+  | -2 -> "rB"
+  | _ -> "r" ^ (string_of_int r)
   (*
   match r with 
   | Register(n) -> "r" ^ (string_of_int n)
